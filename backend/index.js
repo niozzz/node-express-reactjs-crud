16e467c12ee2e5955c35384a96df55a30a71fb72
niozzz <rachmatjackson@gmail.com>
2022-08-12 08:07:27 +0700

    initialize

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use(UserRoute);
+
+app.listen(5000, () => {
+  console.log("server sedang berjalan...");
+});
